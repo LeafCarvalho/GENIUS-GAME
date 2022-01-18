c22172f4de645bcd8d4f9b5af7bba64f73774093
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2022-01-17 22:11:16 -0300

    >

---
+++ move.js
@@ -0,0 +1,7 @@
+let game = document.getElementsByClassName('theGame')
+
+let verde = document.getElementsByClassName('green')
+let vermelho = document.getElementsByClassName('red')
+let amarelo = document.getElementsByClassName('yellow')
+let azul = document.getElementsByClassName('blue')
+
