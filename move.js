552571a7115a9bed0754d66eef18f3fe95055aa9
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2022-01-18 22:37:52 -0300

    consegui fazer a forma toda

--- move.js
+++ move.js
@@ -1,7 +1,6 @@
-let game = document.getElementsByClassName('theGame')
 
-let verde = document.getElementsByClassName('green')
-let vermelho = document.getElementsByClassName('red')
-let amarelo = document.getElementsByClassName('yellow')
-let azul = document.getElementsByClassName('blue')
 
+let verde = document.getElementsByClassName('green');
+let vermelho = document.getElementsByClassName('red');
+let amarelo = document.getElementsByClassName('yellow');
+let azul = document.getElementsByClassName('blue');
