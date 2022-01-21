4d649ce34aad7d6704a05552f686c05ee8d5190f
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2022-01-21 20:05:56 -0300

    quase lá.

--- move.js
+++ move.js
@@ -1,6 +1,95 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
 
+const verde = document.querySelector('green');
+const vermelho = document.querySelector('red');
+const amarelo = document.querySelector('yellow');
+const azul = document.querySelector('blue');
 
-let verde = document.getElementsByClassName('green');
-let vermelho = document.getElementsByClassName('red');
-let amarelo = document.getElementsByClassName('yellow');
-let azul = document.getElementsByClassName('blue');
+let shufflerOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+let lightColor = (element, number) => {
+    time = time * 500
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, tempo - 250);
+    setTimeout(() => {
+        element.classList.remove('selected')
+    })
+}
+
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            lose();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
+}
+
+//funcao para o clique do usuario
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    elementColor(color).classList.add('selected');
+
+    setTimeout(() => {
+        elementColor(color).classList.remove('selected');
+        checkOrder()
+    })
+}
+
+//funcao que retorna a cor
+let createColorElement = (color) => {
+    if(color == 0) {
+        return verde;
+    } else if(color == 1) {
+        return vermelho;
+    } else if(color == 2) {
+        return amarelo;
+    } else if(color == 3) {
+        return azul;
+    }
+}
+
+//funcao para proximo nivel do jogo
+let nextLevel = () => {
+    score++;
+    shufflerOrder();
+}
+
+//funcao para game over
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!`)
+    order = [];
+    clickedOrder = [];
+
+    playGame()
+}
+
+let playGame = () => {
+    alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
+    score = 0;
+
+    nextLevel();
+}
+
+verde.onclick = () => click(0);
+vermelho.onclick = () => click(1);
+amarelo.onclick = () => click(2);
+azul.onclick = () => click(3);
+
+playGame();
